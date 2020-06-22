5019efbe4a1b0ad49f70e4de3f59e3922d761d97
xiaoxiaoweii <2679656468@qq.com>
2020-06-22 22:22:16 +0800

    基础类型和对象类型

---
+++ 2-6基础类型和对象类型/demo.ts
@@ -0,0 +1,23 @@
+// 基础类型
+const counts: number = 123;
+const xiaoxiaoweii: string = 'xiaoxiaoweii';
+
+// 对象类型
+
+class Person {}
+
+const hahaha: {
+  name: string;
+  age: number;
+} = {
+  name: 'xiaoxiaowei',
+  age: 26,
+};
+
+const numberss: number[] = [1,2,3];
+
+const dell: Person = new Person()
+
+const getTotal: () => number = () => {
+  return 123;
+}
