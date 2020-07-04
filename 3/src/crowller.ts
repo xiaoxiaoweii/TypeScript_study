f3cd1c749976c2895f1a98eb6c3162f608aa09e5
xiaoxiaoweii <2679656468@qq.com>
2020-07-04 13:24:35 +0800

    使用cheerio进行数据提取

--- 3/src/crowller.ts
+++ 3/src/crowller.ts
@@ -1,16 +1,43 @@
 // ts  -> .d.ts 翻译文件  -> js
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string,
+  count: number
+}
+
 
 class Crowller {
-  private type = 'movie';
-  private url = `https://${this.type}.douban.com/`;
-  private rawHtml= '';
+  private secret = 'secretKey';
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private rawHtml = '';
+  getJsonInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    const courseInfos: Course[] = [];
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+      courseInfos.push({
+        title,
+        count
+      });
+    });
+    const result = {
+      time: (new Date()).getTime(),
+      data: courseInfos
+    }
+    console.log(result)
+  }
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.rawHtml = result.text
+    this.rawHtml = result.text;
+    this.getJsonInfo(result.text);
   }
   constructor() {
-    this.getRawHtml()
+    this.getRawHtml();
   }
 }
 
