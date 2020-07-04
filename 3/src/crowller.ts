402514c630e1ff1b63b39eaab398e234618d6112
xiaoxiaoweii <2679656468@qq.com>
2020-07-04 17:00:53 +0800

    使用组合设计模式优化代码

--- 3/src/crowller.ts
+++ 3/src/crowller.ts
@@ -1,66 +1,27 @@
 // ts  -> .d.ts 翻译文件  -> js
 import superagent from 'superagent';
-import cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
-
-interface Course {
-  title: string,
-  count: number
-}
-interface courseResult {
-  time: number;
-  data: Course[];
-}
-
-interface Content {
-  [propName: number]: Course[];
-}
+import xiaoxiaoweiiAnalyzer from './xiaoxiaoweiiAnalyzer'
 class Crowller {
-  private secret = 'secretKey';
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  private rawHtml = '';
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html);
-    const courseItems = $('.course-item');
-    const courseInfos: Course[] = [];
-    courseItems.map((index, element) => {
-      const descs = $(element).find('.course-desc');
-      const title = descs.eq(0).text();
-      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
-      courseInfos.push({
-        title,
-        count
-      });
-    });
-    return {
-      time: (new Date()).getTime(),
-      data: courseInfos
-    }
-  }
+  private filePath = path.resolve(__dirname, '../data/course.json');
   async getRawHtml() {
     const result = await superagent.get(this.url);
     return result.text;
   }
+  writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content);
+  }
   async initSpiderProcess() {
-    const filePath = path.resolve(__dirname, '../data/course.json');
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    fs.writeFileSync(filePath,JSON.stringify(fileContent));
+    const fileContent = this.anylyzer.anylyze(html,this.filePath)
+    this.writeFile(fileContent)
   }
-  generateJsonContent(courseInfo: courseResult) {
-    const filePath = path.resolve(__dirname, '../data/course.json');
-    let fileContent: Content = {};
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
-  }
-  constructor() {
+  constructor(private url: string,private anylyzer: any) {
     this.initSpiderProcess();
   }
 }
-
-const crowller = new Crowller();
+const secret = 'secretKey';
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+const anylyzer = new xiaoxiaoweiiAnalyzer();
+new Crowller(url, anylyzer);
