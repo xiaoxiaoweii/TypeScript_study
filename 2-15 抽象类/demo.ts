3516b448a4a95fc4a7b19035b6b873a3db09b826
xiaoxiaoweii <2679656468@qq.com>
2020-07-04 10:36:24 +0800

    抽象类

---
+++ 2-15 抽象类/demo.ts
@@ -0,0 +1,56 @@
+//  readonly
+/* class Person {
+  private readonly name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+const person = new Person('xiaoxiaoweii');
+console.log(person.name); */
+
+// 抽象类
+/* abstract class Geo {
+  width: number;
+  radius: number;
+  getType() {
+    return 'Gemo';
+  }
+  abstract getArea(): number;
+}
+
+class Circle extends Geo {
+  getArea() {
+    return 123;
+  }
+}
+
+class Square {
+  getArea() {}
+}
+
+class Triangle {
+  getArea() {}
+} */
+
+interface teacher {
+  name: string
+}
+interface student {
+  name: string,
+  age: number
+}
+const teacher = {
+  name: "xiaoxiaoweii"
+}
+
+const student = {
+  name: 'xiaoxiao',
+  age: 18
+}
+
+const getUserInfo = (user: teacher | student) => {
+  console.log(user.name);
+}
+
+getUserInfo(teacher)
+getUserInfo(student)
