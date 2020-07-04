26a502ece8067311c18280c86d1b644dc1e1b1ae
xiaoxiaoweii <2679656468@qq.com>
2020-07-04 13:54:46 +0800

    爬虫数据的结构设计和存储

--- 3/src/crowller.ts
+++ 3/src/crowller.ts
@@ -1,18 +1,26 @@
 // ts  -> .d.ts 翻译文件  -> js
 import superagent from 'superagent';
 import cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
 
 interface Course {
   title: string,
   count: number
 }
+interface courseResult {
+  time: number;
+  data: Course[];
+}
 
-
+interface Content {
+  [propName: number]: Course[];
+}
 class Crowller {
   private secret = 'secretKey';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
   private rawHtml = '';
-  getJsonInfo(html: string) {
+  getCourseInfo(html: string) {
     const $ = cheerio.load(html);
     const courseItems = $('.course-item');
     const courseInfos: Course[] = [];
@@ -25,19 +33,33 @@ class Crowller {
         count
       });
     });
-    const result = {
+    return {
       time: (new Date()).getTime(),
       data: courseInfos
     }
-    console.log(result)
   }
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.rawHtml = result.text;
-    this.getJsonInfo(result.text);
+    return result.text;
+  }
+  async initSpiderProcess() {
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    const html = await this.getRawHtml()
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo)
+    fs.writeFileSync(filePath,JSON.stringify(fileContent));
+  }
+  generateJsonContent(courseInfo: courseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
   }
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess();
   }
 }
 
