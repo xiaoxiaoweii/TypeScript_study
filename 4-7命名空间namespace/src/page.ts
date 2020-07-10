2267735b4f1774a207e8c96fefccb98d88e882b1
xiaoxiaoweii <2679656468@qq.com>
2020-07-10 22:48:53 +0800

    命名空间 namespace2

--- 4-7命名空间namespace/src/page.ts
+++ 4-7命名空间namespace/src/page.ts
@@ -1,32 +1,11 @@
+///<reference path='./components.ts'/>
+
 namespace Home {
-	class Header {
-		constructor() {
-			const elem = document.createElement('div');
-			elem.innerText = 'This is Header';
-			document.body.appendChild(elem);
-		}
-	}
-
-	class Content {
-		constructor() {
-			const elem = document.createElement('div');
-			elem.innerText = 'This is Content';
-			document.body.appendChild(elem);
-		}
-	}
-
-	class Footer {
-		constructor() {
-			const elem = document.createElement('div');
-			elem.innerText = 'This is Footer';
-			document.body.appendChild(elem);
-		}
-	}
 	export class Page {
 		constructor() {
-			new Header();
-			new Content();
-			new Footer();
+			new Components.Header();
+			new Components.Content();
+			new Components.Footer();
 		}
 	}
 }
